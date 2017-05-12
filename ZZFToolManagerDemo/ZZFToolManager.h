3b5e6d8ae6f76aee517510fe3b8ee8e65f141ba4
ZZF123 <zhangzhengfeng@zhangzhengfengdeMacBook-Pro.local>
2017-05-12 10:05:44 +0800

    dosomething

---
+++ ZZFToolManagerDemo/ZZFToolManager.h
@@ -0,0 +1,13 @@
+//
+//  ZZFToolManager.h
+//  ZZFToolManagerDemo
+//
+//  Created by 张正峰 on 2017/5/12.
+//  Copyright © 2017年 张正峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZZFToolManager : NSObject
+- (void)doSomething;
+@end
---
+++ ZZFToolManagerDemo/ZZFToolManager.m
@@ -0,0 +1,15 @@
+//
+//  ZZFToolManager.m
+//  ZZFToolManagerDemo
+//
+//  Created by 张正峰 on 2017/5/12.
+//  Copyright © 2017年 张正峰. All rights reserved.
+//
+
+#import "ZZFToolManager.h"
+
+@implementation ZZFToolManager
+- (void)doSomething{
+    NSLog(@"dosomething");
+}
+@end
